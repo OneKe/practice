9552b73241974f39ad31d0437a0e3ccea4372dee
ke <330540431@qq.com>
2020-04-11 23:58:42 +0800

    day20

---
+++ src/test/java/algorithm/day20/Part0887Test.java
@@ -0,0 +1,13 @@
+package algorithm.day20;
+
+import org.junit.Test;
+
+public class Part0887Test {
+
+    @Test
+    public void superEggDrop() {
+        Part0887 test = new Part0887();
+        int ans = test.superEggDrop(1, 2);
+        System.out.println(ans);
+    }
+}
