be69c9df5795fac8c8a1d00a3f4ff6cea51f7441
yangKe <330540431@qq.com>
2020-06-03 14:06:44 +0800

    time test

--- src/test/java/common/UtilTest.java
+++ src/test/java/common/UtilTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
@@ -15,36 +16,33 @@ public class UtilTest {
     @Test
     public void localDateTimeProcess() {
         String start = "2019-09-12 00:00:00";
-        String end = "2020-04-19 23:59:59";
+        String end = "2020-05-19 23:59:59";
         List<String> list = new ArrayList<>();
         String temp1;
         String temp2;
         LocalDateTime localDateTime = LocalDateTime.parse(start, formatter);
+        int year = LocalDateTime.parse(end, formatter).getYear();
+        int month = LocalDateTime.parse(end, formatter).getMonthValue();
         int i = 0;
-        boolean month = !localDateTime.isAfter(LocalDateTime.parse(end, formatter));
-        while (month) {
+        while (!(year == localDateTime.getYear() && month == localDateTime.getMonthValue())) {
             if (i == 0) {
                 temp1 = start;
             } else {
                 temp1 = localDateTime.with(TemporalAdjusters.firstDayOfMonth()).format(formatter);
             }
+            temp2 = LocalDateTime.of(localDateTime.with(TemporalAdjusters.lastDayOfMonth()).toLocalDate(), LocalTime.MAX).format(formatter);
+            list.add(temp1 + "_" + temp2);
             ++i;
             localDateTime = LocalDateTime.parse(start, formatter).plusMonths(i);
-            month = !localDateTime.isAfter(LocalDateTime.parse(end, formatter));
-            if (!month) {
-                temp2 = end;
-            } else {
-                temp2 = localDateTime.with(TemporalAdjusters.lastDayOfMonth()).format(formatter);
-            }
-            list.add(temp1 + "_" + temp2);
-
-            System.out.print(month + ",");
         }
+        temp1 = localDateTime.format(formatter);
+        if (i > 0) {
+            temp1 = localDateTime.with(TemporalAdjusters.firstDayOfMonth()).format(formatter);
+        }
+        temp2 = LocalDateTime.parse(end, formatter).format(formatter);
+        list.add(temp1 + "_" + temp2);
         System.out.println();
-        list.forEach(e -> {
-            System.out.print(e);
-            System.out.println();
-        });
+        list.forEach(System.out::println);
     }
 
     @Test
@@ -54,27 +52,28 @@ public class UtilTest {
         List<String> list = new ArrayList<>();
         String temp1;
         String temp2;
-        LocalDate localDateTime = LocalDate.parse(start);
+        LocalDate localDate = LocalDate.parse(start);
+        int year = LocalDate.parse(end).getYear();
+        int month = LocalDate.parse(end).getMonthValue();
         int i = 0;
-        boolean month = !localDateTime.isAfter(LocalDate.parse(end));
-        while (month) {
+        while (!(year == localDate.getYear() && month == localDate.getMonthValue())) {
             if (i == 0) {
                 temp1 = start;
             } else {
-                temp1 = localDateTime.with(TemporalAdjusters.firstDayOfMonth()).format(DateTimeFormatter.ISO_LOCAL_DATE);
+                temp1 = localDate.with(TemporalAdjusters.firstDayOfMonth()).format(DateTimeFormatter.ISO_LOCAL_DATE);
             }
             ++i;
-            localDateTime = LocalDate.parse(start).plusMonths(i);
-            month = !localDateTime.isAfter(LocalDate.parse(end));
-            if (!month) {
-                temp2 = end;
-            } else {
-                temp2 = localDateTime.with(TemporalAdjusters.lastDayOfMonth()).format(DateTimeFormatter.ISO_LOCAL_DATE);
-            }
+            temp2 = localDate.with(TemporalAdjusters.lastDayOfMonth()).format(DateTimeFormatter.ISO_LOCAL_DATE);
             list.add(temp1 + "_" + temp2);
-
-            System.out.print(month + ",");
+            localDate = LocalDate.parse(start).plusMonths(i);
         }
+        temp1 = start;
+        if (i > 0) {
+            temp1 = localDate.with(TemporalAdjusters.firstDayOfMonth()).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        }
+        temp2 = LocalDate.parse(end).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        list.add(temp1 + "_" + temp2);
+
         System.out.println();
         list.forEach(e -> {
             System.out.print(e);
