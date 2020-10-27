a022c955c692742378a447f63ca521590ab169be
ke <330540431@qq.com>
2020-10-27 23:58:05 +0800

    algorithm

---
+++ src/main/java/algorithm/day93/Part0144.java
@@ -0,0 +1,40 @@
+package algorithm.day93;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 144. 二叉树的前序遍历
+ * <p>
+ * 给定一个二叉树，返回它的 前序 遍历。
+ */
+public class Part0144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode p = root;
+        List<Integer> res = new ArrayList<>();
+        while (p != null || !stack.isEmpty()) {
+            while (p != null) {
+                res.add(p.val);
+                stack.push(p);
+                p = p.left;
+            }
+            p = stack.pop().right;
+        }
+        return res;
+    }
+      public static class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+}
